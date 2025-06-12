c2a79e2d38c92d6b3aab19ccafdc1286276a5e4d
ELResidenteX <yasuogbx@gmail.com>
2025-06-12 18:36:57 -0400

    Corrijo ruta de login para template en usuarios/login.html

--- SistemasPoliciales/urls.py
+++ SistemasPoliciales/urls.py
@@ -17,12 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import path, include
+from django.contrib.auth import views as auth_views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),  # Ruta principal
     path('infracciones/', include('infracciones.urls')),
     path('funcionarios/', include('funcionarios.urls')),
+    path('login/', auth_views.LoginView.as_view(template_name='usuarios/login.html'), name='login'),
 
 
 ]
