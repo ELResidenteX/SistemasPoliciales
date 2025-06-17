90c9af884e83a636b51612de47a6b752ccce5ee2
ELResidenteX <yasuogbx@gmail.com>
2025-06-16 21:50:39 -0400

    Configura Django para usar AWS S3 usando variables de entorno

--- SistemasPoliciales/settings.py
+++ SistemasPoliciales/settings.py
@@ -137,7 +137,7 @@ if not DEBUG:
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-    AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+    AWS_STORAGE_BUCKET_NAME = os.getenv('firmassistemapolicial')
     AWS_S3_REGION_NAME = "sa-east-1"  # Cambia si tu bucket es de otra región
     AWS_S3_SIGNATURE_VERSION = "s3v4"
     AWS_S3_FILE_OVERWRITE = False
