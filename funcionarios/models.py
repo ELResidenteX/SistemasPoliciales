a047ef2d28ede0385abc59447b68e636465e6c3d
ELResidenteX <yasuogbx@gmail.com>
2025-06-25 18:54:37 -0400

    🛠️ Agrega campo 'cambio_password_obligado' al modelo PerfilUsuario para forzar cambio de contraseña

--- funcionarios/models.py
+++ funcionarios/models.py
@@ -17,5 +17,9 @@ class PerfilUsuario(models.Model):
     rol = models.CharField(max_length=20, choices=ROLES)
     rut = models.CharField(max_length=12, unique=True)
 
+
+    # ✅ Campo nuevo para controlar si debe cambiar su contraseña
+    cambio_password_obligado = models.BooleanField(default=True)
+
     def __str__(self):
         return f"{self.user.username} - {self.get_rol_display()}"
