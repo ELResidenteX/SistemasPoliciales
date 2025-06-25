b26cab8da14903a466673e155611237c8c8d42d9
ELResidenteX <yasuogbx@gmail.com>
2025-06-25 14:32:24 -0400

    ✅ Agrega ruta forzar_cambio_password para redirección obligatoria en primer login

--- funcionarios/urls.py
+++ funcionarios/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import login_view, logout_view, vista_inicio, admin_login_view
-from .views import crear_usuario,  post_login
+from .views import crear_usuario,  post_login, forzar_cambio_password
 
 urlpatterns = [
     path('login/', login_view, name='login'),
@@ -9,7 +9,7 @@ urlpatterns = [
     path('usuarios/crear/', crear_usuario, name='crear_usuario'),
     path('admin-login/', admin_login_view, name='admin_login'),
     path('usuarios/post_login/', post_login, name='post_login'),
-
+    path('forzar-cambio-password/', forzar_cambio_password, name='forzar_cambio_password'),
 
 
 ]
