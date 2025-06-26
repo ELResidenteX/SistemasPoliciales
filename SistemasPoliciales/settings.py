119cc348c8c6f3c6895942c26ca248a5d2d3602c
ELResidenteX <yasuogbx@gmail.com>
2025-06-25 20:30:24 -0400

    ⚙️ Registra middleware personalizado para control de sesiones y vencimiento de contraseña

--- SistemasPoliciales/settings.py
+++ SistemasPoliciales/settings.py
@@ -47,6 +47,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'middleware.verificar_tiempo_password.ExpulsarSiNoCambioPasswordMiddleware',
+
 ]
 
 ROOT_URLCONF = 'SistemasPoliciales.urls'
